3863579134b554ba9a7cdf1b906a723c6331de3a
keitakn <keita.koga.work@gmail.com>
2025-10-20 13:38:12 +0900

    :lipstick: #3 ライトモードにして余計なCSSを削除

--- ai-agent-sandbox-frontend/src/app/layout.tsx
+++ ai-agent-sandbox-frontend/src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja" className='dark'>
+    <html lang="ja" className='light'>
       <body>
         <Providers>
           {children}
