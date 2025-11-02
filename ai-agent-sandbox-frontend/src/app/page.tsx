cb3118486d01f30a1e9a5e48f806dc42a381c679
keitakn <keita.koga.work@gmail.com>
2025-11-02 15:45:08 +0900

    :recycle: #2 Componentを移動

--- ai-agent-sandbox-frontend/src/app/page.tsx
+++ ai-agent-sandbox-frontend/src/app/page.tsx
@@ -1,5 +1,5 @@
 import SimpleNavbar from "@/components/heroui/pro/simple-navbar";
-import { Assistant } from "@/features/components/assistant";
+import { Assistant } from "@/features/components/ai-agents/assistant";
 
 export default function Home() {
   return (
